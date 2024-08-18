ec66703ccc073cf758d3b317b49461a73896ac64
jinexus1 <ap2929470@gmail.com>
2024-08-18 17:34:21 +0530

    Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
-import React,{useState} from 'react'
-import './Home.css'
-import Header from '../../components/Header/Header'
+import React, { useState } from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 const Home = () => {
-  const [category,setCategory]=useState("All")
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
-      <ExploreMenu  category={category} setCategory={setCategory}/>
+      <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category}></FoodDisplay>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
